a39031d648cea419a5a4b194fae09fda8c0b2691
kebrorba <abror.esonaliyev@gmail.com>
2023-01-28 14:17:53 +0500

    shared flows are added

---
+++ mobile/src/main/kotlin/uz/easycongroup/smartenergy/domain/sharedflow/global/date/DateSelectionSharedFlow.kt
@@ -0,0 +1,5 @@
+package uz.easycongroup.smartenergy.domain.sharedflow.global.date
+
+import uz.easycongroup.smartenergy.core.domain.corountines.MutableSharedFlowWrapper
+
+class DateSelectionSharedFlow : MutableSharedFlowWrapper<Long>()
---
+++ mobile/src/main/kotlin/uz/easycongroup/smartenergy/domain/sharedflow/global/sorttype/SortTypeSharedFlow.kt
@@ -0,0 +1,6 @@
+package uz.easycongroup.smartenergy.domain.sharedflow.global.sorttype
+
+import uz.easycongroup.smartenergy.core.domain.corountines.MutableSharedFlowWrapper
+import uz.easycongroup.smartenergy.domain.data.models.sort.SortType
+
+class SortTypeSharedFlow : MutableSharedFlowWrapper<SortType?>()
