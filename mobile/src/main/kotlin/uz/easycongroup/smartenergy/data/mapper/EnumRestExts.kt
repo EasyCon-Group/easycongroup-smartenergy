74781ce8d28989f14a011e2a536c37223b6ceb7c
kebrorba <abror.esonaliyev@gmail.com>
2023-01-28 14:09:41 +0500

    models and mappers are added 3

--- mobile/src/main/kotlin/uz/easycongroup/smartenergy/data/mapper/EnumRestExts.kt
+++ mobile/src/main/kotlin/uz/easycongroup/smartenergy/data/mapper/EnumRestExts.kt
@@ -1,21 +1,2 @@
 package uz.easycongroup.smartenergy.data.mapper
 
-import uz.easycongroup.smartenergy.domain.data.models.rest.RemoteConstants.CONST_SORT_BY_DISCOUNT
-import uz.easycongroup.smartenergy.domain.data.models.rest.RemoteConstants.CONST_SORT_BY_NEW
-import uz.easycongroup.smartenergy.domain.data.models.rest.RemoteConstants.CONST_SORT_BY_POPULARITY
-import uz.easycongroup.smartenergy.domain.data.models.rest.RemoteConstants.CONST_SORT_BY_PRICE
-import uz.easycongroup.smartenergy.domain.data.models.rest.RemoteConstants.CONST_SORT_BY_RATING
-import uz.easycongroup.smartenergy.domain.data.models.rest.RemoteConstants.CONST_SORT_BY_REVIEW
-import uz.easycongroup.smartenergy.domain.data.models.sort.SortType
-import uz.easycongroup.smartenergy.domain.data.models.sort.SortType.*
-
-val SortType?.restCode: String
-    get() = when (this) {
-        null -> ""
-        SORT_BY_POPULARITY -> CONST_SORT_BY_POPULARITY
-        SORT_BY_PRICE -> CONST_SORT_BY_PRICE
-        SORT_BY_RATING -> CONST_SORT_BY_RATING
-        SORT_BY_REVIEW -> CONST_SORT_BY_REVIEW
-        SORT_BY_DISCOUNT -> CONST_SORT_BY_DISCOUNT
-        SORT_BY_NEW -> CONST_SORT_BY_NEW
-    }
--- mobile/src/main/kotlin/uz/easycongroup/smartenergy/domain/data/models/dashboard/detail/ProductWarehouse.kt
+++
@@ -1,8 +0,0 @@
-package uz.easycongroup.smartenergy.domain.data.models.dashboard.detail
-
-data class ProductWarehouse(
-    val id: Long,
-    val regionId: Long,
-    val districtId: Long,
-    val address: String
-)
